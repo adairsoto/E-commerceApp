bc6d65f973fc6b5b8b6f121786e604d29fe7d662
Adair Soto <91437559+adairsoto@users.noreply.github.com>
2023-04-16 23:22:26 -0300

    End of Section 6

--- API/Extesions/ApplicationServicesExtensions.cs
+++ API/Extesions/ApplicationServicesExtensions.cs
@@ -37,6 +37,15 @@ namespace API.Extesions
                     return new BadRequestObjectResult(errorResponse);
                 };
             });
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            }); 
+
             return services;
         }
     }
