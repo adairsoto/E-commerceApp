763a8158d5755239a24addd81c347ab490c5881c
adairsoto <adair.soto@protonmail.com>
2024-04-20 08:26:26 -0300

    Add API Basket

--- Infrastructure/obj/Debug/net7.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net7.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b47c728d59af7a66c71a9ab09f272e5893b817d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e4e73378815a431aacebed7bbaaf233478ce687")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
